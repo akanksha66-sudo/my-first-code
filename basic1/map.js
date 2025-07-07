d25cac1f8f3b684037f82b1df7f358b462f15353
akanksha66-sudo <akanksharai032@gmail.com>
2025-07-07 09:27:19 +0000

    map.js

---
+++ basic1/map.js
@@ -0,0 +1,15 @@
+const myNum = [1,2,3,4,5,6,7,8,9,10]
+
+//const number = myNum.map( (num) => num + 10)
+
+const number = myNum
+
+.map((num) => num *10  )
+
+.map((num) => num +1 )
+
+console.log(number)
+
+
+
+
